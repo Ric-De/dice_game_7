422ff4176eab6801c77e363f8a8c9bafdc817177
Ric-De <ricdellazari@gmail.com>
2024-02-12 01:21:38 +0100

    Feature-User methods and table adds

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    //One to many relationships
+    public function games(){
+        return $this->hasMany(Game::class);
+    }
 }
