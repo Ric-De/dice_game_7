497cc234adf642cc6a0741c075a320ffde676f74
Ric-De <ricdellazari@gmail.com>
2024-02-12 01:32:15 +0100

    Feature-GameContr methods Gamemodel/table/routes

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,27 @@ Route::controller(ApiController::class)->group(function() {
 Route::middleware('auth:api')->group( function () {
     Route::post('/logout', [ApiController::class, 'logout'])->name('user.logout');
 });
+
+Route::middleware('auth:api')->group(function(){
+
+    //Admin routes
+    Route::get('/players', [UserController::class, 'index'])->middleware('can:index')->name('user.index'); 
+    
+    Route::get('/players/ranking', [UserController::class, 'getRanking'])->middleware('can:getRanking')->name('user.ranking'); 
+    
+    Route::get('/players/ranking/loser', [UserController::class, 'getLoser'])->middleware('can:getLoser')->name('user.loser');
+    
+    Route::get('/players/ranking/winner', [UserController::class, 'getWinner'])->middleware('can:getWinner')->name('user.winner');
+    
+    
+    //Player routes
+    Route::post('/players/{id}/games/',[GameController::class,'play'])->middleware('can:play')->name('games.play');
+    
+    Route::get('players/{id}/games/',[GameController::class,'index_games'])->middleware('can:index_games')->name('games.index');
+    
+    Route::put('/players/{id}/', [UserController::class, 'update'])->middleware('can:update')->name('user.update');
+    
+    Route::delete('/players/{id}/games/',[GameController::class,'destroy'])->middleware('can:destroy')->name('games.destroy');
+    
+
+});
