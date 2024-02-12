2fec1a5bf235e86312f4e105d9c14e011ff15a3b
Ric-De <ricdellazari@gmail.com>
2024-02-12 01:42:02 +0100

    Feature-RolesPermissions, seeders and factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,43 +2,38 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * The name of the factory's corresponding model.
+     *
+     * @var string
      */
-    protected static ?string $password;
+    protected $model = User::class;
 
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->unique()->word,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('123456789'),
         ];
     }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
 }
+
+    User::created(function (User $user) {
+        $playerRole = Role::where('name', 'player')->first();
+        $user->assignRole($playerRole);
+    });
+    
+?>
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Game;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +18,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //Populating game and user tables with fakers. Calling the RoleSeeder.
+        User::factory(10)->create();
+        Game::factory(10)->create();
+        $this->call(RoleSeeder::class);
+        
+        //Creating a default Admin
+        $defaultAdmin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt(123456789),
+            'remember_token' => Str::random(10),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $defaultAdmin->assignRole('admin');
+
+        //Creating a default Player
+        $defaultPlayer = User::create([
+            'name' => 'player',
+            'email' => 'player@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt(987654321),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $defaultPlayer->assignRole('player');
+
+
     }
 }
